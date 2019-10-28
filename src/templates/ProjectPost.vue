4ac1216c967ec87673ca4b4a3adaedc5d3f2a9ea
dalton.s <dalton.s@iconinteractive.com>
2019-10-28 17:47:23 -0400

    add about

--- src/templates/ProjectPost.vue
+++ src/templates/ProjectPost.vue
@@ -1,19 +1,16 @@
 <template>
   <Layout>
     <div class="project">
-
       <div class="container">
-
         <div class="project-header">
           <h1 class="project-title" v-html="$page.post.title" />
           <div class="project-info">
-
             <div class="categories-container">
               <div class="categories">
                 <span class="label">Categories</span>
-                <span 
+                <span
                   class="category"
-                  v-for="(category, index) in $page.post.categories" 
+                  v-for="(category, index) in $page.post.categories"
                   :key="index"
                   v-text="category"
                 />
@@ -22,15 +19,13 @@
 
             <div class="year-container">
               <span class="label">Year</span>
-              <div v-html="$page.post.date"/>
+              <div v-html="$page.post.date" />
             </div>
           </div>
         </div>
 
         <div v-html="$page.post.content" class="content" />
-
       </div>
-
     </div>
   </Layout>
 </template>
@@ -42,23 +37,18 @@ query ProjectPost ($path: String!) {
     date (format: "YYYY")
     content
     categories
-    project_bg_color
-    project_fg_color
   }
 }
 </page-query>
 
 <script>
 export default {
-  metaInfo () {
+  metaInfo() {
     return {
-      title: this.$page.post.title,
-      bodyAttrs: {
-        style: `background-color: ${this.$page.post.project_bg_color ? this.$page.post.project_bg_color : 'var(--color-base)'}; color: ${this.$page.post.project_fg_color ? this.$page.post.project_fg_color : 'var(--color-contrast)'}`
-      }
-    }
+      title: this.$page.post.title
+    };
   }
-}
+};
 </script>
 
 <style scoped>
@@ -82,9 +72,9 @@ export default {
   margin: 0;
 }
 .category:after {
-  content: ', '
+  content: ", ";
 }
 .category:last-of-type:after {
-  content: '';
+  content: "";
 }
 </style>
