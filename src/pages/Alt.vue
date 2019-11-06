d5c45e1a657984b9f95b3739660e3ef2506d53ba
daltn <daltonsaffe@gmail.com>
2019-11-06 14:18:10 -0500

    add alt page

---
+++ src/pages/Alt.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <div style="padding:56.25% 0 0 0;position:relative;">
+      <iframe
+        src="https://player.vimeo.com/video/89785909?autoplay=1&title=0&byline=0&portrait=0"
+        style="position:absolute;top:0;left:0;width:100%;height:100%;"
+        frameborder="0"
+        allow="autoplay; fullscreen"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <Layout>
+      <div class="container spacer">
+        <ProjectsGrid :projects="$page.projects.edges" />
+      </div>
+    </Layout>
+  </section>
+</template>
+
+<page-query>
+query Posts {
+	projects: allProjectPost {
+    edges {
+      node {
+        id
+        date (format: "YYYY")
+        title
+        categories
+        thumbnail
+        path
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import Hero from "@/components/Hero";
+import ProjectsGrid from "@/components/ProjectsGrid";
+
+export default {
+  components: {
+    Hero,
+    ProjectsGrid
+  }
+};
+</script>
+
+<style scoped>
+.spacer {
+  margin-top: 10rem;
+}
+</style>
