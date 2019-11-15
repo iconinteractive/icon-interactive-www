fa6e1349979d39eebecb3dd59457e92c821ccaea
daltn <daltonsaffe@gmail.com>
2019-11-15 14:49:13 -0500

    slice out logo grid

---
+++ src/components/LogoGrid.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="grid">
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/audi.jpg" alt="audi logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/beaste-boys.png" alt="beastie boys logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/bmw.svg" alt="bmw logo" class="mini-logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/bonnaroo.png" alt="bonnaroo logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/jlo.svg" alt="jlo logo" class="mini-logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/katy.png" alt="katy logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/land-rover.png" alt="land rover logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/nissan.svg" alt="nissan logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/panasonic.jpg" alt="panasonic logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/phish.jpg" alt="phish logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/siriusxm.jpg" alt="siriusxm logo" />
+    </div>
+    <div class="logo-wrap">
+      <g-image src="~/assets/logos/toyota.png" alt="toyota logo" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  justify-items: center;
+  grid-gap: 2rem;
+}
+.grid img {
+  width: 120px;
+}
+.logo-wrap {
+  display: flex;
+  align-self: center;
+}
+.mini-logo {
+  height: 80px;
+}
+</style>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -4,55 +4,7 @@
       <h1 class="title">Work</h1>
       <ProjectsGrid :projects="$page.projects.edges" />
       <h1 class="title">Clients</h1>
-      <section class="grid">
-        <div class="logo-wrap">
-          <g-image src="~/assets/logos/audi.jpg" alt="audi logo" />
-        </div>
-        <div class="logo-wrap">
-          <g-image
-            src="~/assets/logos/beaste-boys.png"
-            alt="beastie boys logo"
-          />
-        </div>
-        <div class="logo-wrap">
-          <g-image
-            src="~/assets/logos/bmw.svg"
-            alt="bmw logo"
-            class="mini-logo"
-          />
-        </div>
-        <div class="logo-wrap">
-          <g-image src="~/assets/logos/bonnaroo.png" alt="bonnaroo logo" />
-        </div>
-        <div class="logo-wrap">
-          <g-image
-            src="~/assets/logos/jlo.svg"
-            alt="jlo logo"
-            class="mini-logo"
-          />
-        </div>
-        <div class="logo-wrap">
-          <g-image src="~/assets/logos/katy.png" alt="katy logo" />
-        </div>
-        <div class="logo-wrap">
-          <g-image src="~/assets/logos/land-rover.png" alt="land rover logo" />
-        </div>
-        <div class="logo-wrap">
-          <g-image src="~/assets/logos/nissan.svg" alt="nissan logo" />
-        </div>
-        <div class="logo-wrap">
-          <g-image src="~/assets/logos/panasonic.jpg" alt="panasonic logo" />
-        </div>
-        <div class="logo-wrap">
-          <g-image src="~/assets/logos/phish.jpg" alt="phish logo" />
-        </div>
-        <div class="logo-wrap">
-          <g-image src="~/assets/logos/siriusxm.jpg" alt="siriusxm logo" />
-        </div>
-        <div class="logo-wrap">
-          <g-image src="~/assets/logos/toyota.png" alt="toyota logo" />
-        </div>
-      </section>
+      <LogoGrid />
     </div>
   </Layout>
 </template>
@@ -76,29 +28,14 @@ query Posts {
 
 <script>
 import ProjectsGrid from "@/components/ProjectsGrid";
+import LogoGrid from "@/components/LogoGrid";
 
 export default {
   components: {
-    ProjectsGrid
+    ProjectsGrid,
+    LogoGrid
   }
 };
 </script>
 
-<style scoped>
-.grid {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  justify-items: center;
-  grid-gap: 2rem;
-}
-.grid img {
-  width: 120px;
-}
-.logo-wrap {
-  display: flex;
-  align-self: center;
-}
-.mini-logo {
-  height: 80px;
-}
-</style>
+
