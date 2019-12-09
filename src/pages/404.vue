e57b29e5474ebbbaf1b3684f52f5131914a46534
daltn <daltonsaffe@gmail.com>
2019-12-09 15:41:53 -0500

    add redirect file

---
+++ src/pages/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <section class="container">
+      <h1>404 ~ Not Found 😔</h1>
+      <img
+        src="https://media.giphy.com/media/qqsvBtsIfvUxG/giphy.gif"
+        alt="windows xp gif"
+        width="50%"
+      />
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
