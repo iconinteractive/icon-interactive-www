c8b04cc7ae7796a4271bba40cec5795f58f8589d
daltn <daltonsaffe@gmail.com>
2020-01-15 17:02:22 -0500

    init pdf viewer

---
+++ src/templates/PdfPost.vue
@@ -0,0 +1,66 @@
+<template>
+  <Layout>
+    <div class="container"></div>
+  </Layout>
+</template>
+
+<page-query>
+// query NewsPost ($path: String!) {
+//   post: newsPost (path: $path) {
+//     title
+//     date (format: "YYYY")
+//     content
+//   }
+// }
+</page-query>
+
+<script>
+export default {
+  components: {},
+  metaInfo() {
+    return {
+      title: this.$page.post.title
+    };
+  }
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: underline;
+  color: var(--color-highlight);
+}
+.event-content {
+  width: 75%;
+}
+.event-header {
+  padding: 2rem 0 4rem 0;
+}
+.title {
+  margin: 0 0 4rem 0;
+  padding: 0;
+  line-height: 5.25rem;
+}
+.event-meta {
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 1rem;
+}
+.event-meta > div {
+  margin-right: 4rem;
+}
+.event-meta > div:last-of-type {
+  margin: 0;
+}
+.label {
+  color: var(--color-highlight);
+}
+@media (max-width: 800px) {
+  .event-content {
+    width: 95%;
+  }
+  .title {
+    line-height: 3rem;
+  }
+}
+</style>
