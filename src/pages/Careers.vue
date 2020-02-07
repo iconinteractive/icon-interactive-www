d3a1feaca3124bd31787c455967d80ba25e02293
dalton.s <dalton.s@iconinteractive.com>
2020-02-07 11:10:29 -0500

    mod careers iframe url

--- src/pages/Careers.vue
+++ src/pages/Careers.vue
@@ -14,7 +14,7 @@ export default {
       let elem = document.createElement("script");
       elem.type = "text/javascript";
       elem.src =
-        "http://newton.newtonsoftware.com/career/iframe.action?clientId=8a7883c6700909c4017010fafd6c06e7";
+        "https://newton.newtonsoftware.com/career/iframe.action?clientId=8a7883c6700909c4017010fafd6c06e7";
 
       let wrapper = document.querySelector("#career-frame");
       wrapper.appendChild(elem);
