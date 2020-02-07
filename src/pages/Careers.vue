1421ae12739a37b557f9028315fcc43850dd3005
dalton.s <dalton.s@iconinteractive.com>
2020-02-07 11:34:32 -0500

    mod script embed

--- src/pages/Careers.vue
+++ src/pages/Careers.vue
@@ -2,26 +2,45 @@
   <Layout>
     <section class="container">
       <h1>Careers</h1>
-      <div id="career-frame"></div>
+      <div ref="snippet_container"></div>
     </section>
   </Layout>
 </template>
 
 <script>
+let htmlEl = null;
 export default {
   mounted() {
     if (process.isClient) {
-      let elem = document.createElement("script");
-      elem.type = "text/javascript";
-      elem.src =
-        "https://newton.newtonsoftware.com/career/iframe.action?clientId=8a7883c6700909c4017010fafd6c06e7";
+      insertSnippet();
+      // let elem = document.createElement("script");
+      // elem.type = "text/javascript";
+      // elem.src =
+      //   "https://newton.newtonsoftware.com/career/iframe.action?clientId=8a7883c6700909c4017010fafd6c06e7";
 
-      let wrapper = document.querySelector("#career-frame");
-      wrapper.appendChild(elem);
+      // let wrapper = document.querySelector("#career-frame");
+      // wrapper.appendChild(elem);
+    }
+  },
+  methods: {
+    insertSnippet() {
+      if (htmlEl) return;
+      const el = this.$refs.snippet_container;
+      htmlEl = document.createElement("div");
+      htmlEl.classList.add("html-snippet");
+      el.appendChild(htmlEl);
+      const scriptsTags = htmlEl.getElementsByTagName("script");
+      for (let i = 0; i < scriptsTags.length; i++) {
+        let parentNode = scriptsTags[i].parentNode;
+        let newScriptTag = document.createElement("script");
+        newScriptTag.type = "text/javascript";
+        newScriptTag.src =
+          "https://newton.newtonsoftware.com/career/iframe.action?clientId=8a7883c6700909c4017010fafd6c06e7";
+        newScriptTag.text = scriptsTags[i].text;
+        parentNode.removeChild(scriptsTags[i]);
+        parentNode.appendChild(newScriptTag);
+      }
     }
   }
 };
 </script>
-
-<style>
-</style>
