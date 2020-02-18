7ddbc78cbfcdf4a3697828b6829c88762035fdde
dalton.s <dalton.s@iconinteractive.com>
2020-02-18 17:18:52 -0500

    fix icon home header

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -57,13 +57,16 @@ export default {
 .hero {
   text-align: left;
   padding-left: 125px;
+  -webkit-hyphens: auto;
+  -ms-hyphens: auto;
   hyphens: auto;
   height: 100%;
+  max-width: 850px;
   text-align: left;
   margin-bottom: 0;
 }
 .hero-title {
-  font-size: 7.5rem;
+  font-size: 7rem;
   letter-spacing: 2px;
   font-weight: 700;
   padding: 0;
@@ -72,6 +75,7 @@ export default {
   -ms-hyphens: auto;
   hyphens: auto;
   line-height: 6.75rem;
+  
 }
 .pink {
   color: var(--color-highlight);
@@ -79,10 +83,11 @@ export default {
   -ms-hyphens: auto;
   hyphens: auto;
 }
-@media (max-width: 800px) {
+@media (max-width: 900px) {
   .hero {
     padding-left: 0;
     margin: 2rem 0;
+    width: 70%;
   }
   .hero-title {
     font-size: 2.75rem;
