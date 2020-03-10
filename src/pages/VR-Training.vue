a2cef68b8bdceddd0e8c11af7f8f1a95312cb0bf
Edwin Condori <ovidio43@gmail.com>
2020-03-10 17:44:59 -0400

    add new page

---
+++ src/pages/VR-Training.vue
@@ -0,0 +1,218 @@
+<template>
+  <Layout>
+      <section>
+        <div class="vr-section-1">
+          <div class="container">
+            <ul class="grid-two">
+              <li>
+              <i class="line-title white"></i>
+              <h2>Learn how to deploy a scalable and effective virtual reality (VR) training solution with a return on investment</h2>
+              <p>Presented By ICON Interactive</p>               
+              </li>
+            </ul>
+          </div>          
+        </div>
+        <div class="vr-section-2">
+          <div class="container">
+            <i class="line-title white"></i>
+            <h2>The following presents a look at the effectiveness of VR training and how it can be implemented in your company.</h2>
+            <h2>Learn how you can...</h2>
+            <p>Provide effective training for skills related to remembering and understanding spatial and visual information.</p>
+            <p>Leverage VR for effective training of cognitive, motor, and soft skills.</p>
+            <p>Access the emotional areas of the brain typically only activated in real-life situations, allowing you to reproduce virtual training that was previously only possible in real environments.</p>
+            <p>Deliver real-world experiences with the scalability of digital technology.   </p>       
+          </div>          
+        </div>  
+        <div class="vr-section-3">
+          <div class="container">
+            <i class="line-title bicolor"></i>
+            <h2>Realistic, immersive simulations allow trainees to learn in a safe environment, with no physical danger or potential costly damage to equipment.</h2>
+            <p>Immersive experiences increase engagement and with the “freedom to fail”, outcomes improve which in turn increases motivation to learn and retain new information.</p>
+            <p>VR allows for self-paced and distance learning, making it convenient for both trainees and trainers.</p>            
+          </div>          
+        </div>  
+        <div class="vr-section-4 vr-white">
+          <div class="container">
+            <ul class="grid-two">
+              <li>
+             <i class="line-title bicolor"></i>
+              <h2>The Importance of Presence</h2>
+              <p><b>The potential of VR training lies in its power to deliver physical, real-world experiences, with the scalability and accessibility of digital technology.</b></p>
+              <p>In the real world, you feel “presence”, which is your subjective experience of reality. Presence allows you to navigate in an environment in which your sensory input responds to your movements and actions. This coordinated system helps your brain determine what is real.</p>
+              <p>VR provides a way to deliver enough synchronous sensory input to confuse your brain into thinking that what is happening inside a virtual environment is real. This allows for the benefits of “in-person” interactions to be distributed digitally across your organization.</p>             
+              </li>
+            </ul>            
+
+          </div>          
+        </div>  
+        <div class="vr-section-5 vr-white">
+          <div class="container">
+            <ul class="grid-two">
+              <li>
+             <i class="line-title bicolor"></i>
+              <h2>Advantages of VR</h2>
+              <h3>VR is More Memorable</h3>
+              <p>VR is highly effective as a training tool due to varying levels of presence. Your ability to retain and recall a subject after a VR experience tends to be higher than after viewing video- or text-based materials.</p>
+              <p>Your memory is better when there is increased multi-sensory and emotional input. So when you reflect on a VR experience, it tends to feel more like a memory of something that actually happened.</p>          
+              </li>
+            </ul>              
+          </div>          
+        </div>
+        <div class="vr-section-6 vr-white">
+          <div class="container">
+            <ul class="grid-two">
+              <li>
+             <i class="line-title bicolor"></i>
+              <h2>Advantages of VR</h2>
+              <h3>VR is Repeatable</h3>
+              <p>In a VR experience, trainees can be repeatedly exposed to a stressful and/or dangerous situation, while under safe conditions. Over time, this can reduce the stress response to that situation. </p>
+              <p>Because the experience is virtual, trainees can be subjected to varying levels of intensity allowing them to gradually become accustomed to more intense situations.</p>          
+              </li>
+            </ul>              
+          </div>          
+        </div>  
+        <div class="vr-section-7 vr-white">
+          <div class="container">
+            <ul class="grid-two">
+              <li>
+             <i class="line-title bicolor"></i>
+              <h2>Advantages of VR</h2>
+              <h3>VR Provides Isolation From Distractions</h3>
+              <p>VR headsets allow individuals to focus on the task at hand by providing freedom from distractions, while still allowing for virtual collaboration with other trainees and instructors. </p>
+              <p>The more engagement and interactivity a trainee experiences, the less likely they are to feel the need to take calls, check messages and scroll through their digital life.</p>          
+              </li>
+            </ul>              
+          </div>          
+        </div>   
+        <div class="vr-section-8 vr-white">
+          <div class="container">
+            <ul class="grid-two">
+              <li>
+             <i class="line-title bicolor"></i>
+              <h2>Advantages of VR</h2>
+              <h3>VR is Safer</h3>
+              <p>VR facilitates training in a physical environment without the safety risks to the trainee or expensive equipment. </p>
+              <p>If a trainee becomes overwhelmed, the experience can be adjusted to be less overwhelming or they can simply take off the headset.</p> 
+              <p>VR allows your trainees to practice before moving on to real-world training situations that have safety risks.</p>         
+              </li>
+            </ul>              
+          </div>          
+        </div> 
+        <div class="vr-section-9 vr-white">
+          <div class="container">
+            <i class="line-title bicolor"></i>
+            <h2>Skills Suited For VR Training</h2>
+            <ul class="grid-three">
+              <li>
+                <h3>Cognitive Skills</h3>
+<p>Cognitive skills are the core skills your brain uses to think, read, learn, remember, reason, and pay attention.</p>
+<p>Working together, they take incoming information and move it into the bank of knowledge you use every day.</p>
+              </li>
+              <li>
+<h3>Psychomotor Skills</h3>
+<p>Psychomotor skills represent the relationship between cognitive functions and physical movement.</p>
+<p>Psychomotor learning is demonstrated by physical skills such as movement, coordination, manipulation, dexterity, grace, strength and speed.</p>
+                
+              </li>
+              <li>
+<h3>Soft Skills</h3>
+<p>Soft skills are the interpersonal and social skills needed to control feelings and emotions.</p>
+<p>Practicing real-world situations in the safety of a virtual world allows trainees to learn through experience. </p>               
+              </li>
+            </ul>
+          </div>          
+        </div>                                                           
+      </section>
+
+
+
+                       
+
+      <section class="vr-section-7">
+        <div class="container">
+        </div>          
+      </section>
+      <section class="vr-section-8">
+        <div class="container">
+        </div>          
+      </section>
+      <section class="vr-section-9">
+        <div class="container">
+        </div>          
+      </section>
+      <section class="vr-section-10">
+        <div class="container">
+        </div>          
+      </section>
+      <section class="vr-section-11">
+        <div class="container">
+        </div>          
+      </section>      
+
+
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+  .vr-section-1{
+    color: rgb(255, 255, 255);
+    background:var(--color-highlight);
+    background: linear-gradient(to right bottom, var(--color-highlight) 50%, rgb(255, 255, 255) 50%);    
+    padding: 5em 0;
+    min-height: 85vh;
+  }
+  .vr-section-2{
+    background:var(--color-highlight);
+    color: rgb(255, 255, 255);
+    padding: 5em 0;
+  }
+  .vr-section-3{
+    background-color:rgba(0,0,0,0.7);
+    color: rgb(255, 255, 255);
+    padding: 5em 0;    
+  }
+  .vr-white{
+    background-color:rgba(255,255,255,1);
+    color: rgb(0, 0, 0);
+    padding: 5em 0;      
+  }
+  .grid-two {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 3rem;
+    list-style: none;
+    padding: 0;
+  }
+  .grid-three {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 3rem;
+    list-style: none;
+    padding: 0;
+  }  
+  .line-title{
+    height: 8px;
+    width: 100px;
+    background-color:  var(--color-highlight);
+    clear: both;
+    margin-bottom: 1em;
+    display: block;
+  }
+  .line-title.white{
+    background-color: white;
+  }
+  .line-title.bicolor{
+    background:var(--color-highlight);
+    background: linear-gradient(to  left, var(--color-highlight) 50%, grey 50%); 
+  }
+  h2{
+    margin-bottom: 1.3em;
+  }
+  p{
+    margin-bottom: 1.3em;
+  }
+</style>
