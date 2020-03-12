0aa6c188e0b49c39682191ea9dea89c5dd95ac6d
eddy <ovidio43@gmail.com>
2020-03-11 22:31:20 -0400

    add

--- src/pages/VR-Training.vue
+++ src/pages/VR-Training.vue
@@ -5,19 +5,25 @@
           <div class="container">
             <ul class="grid-two">
               <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/logo-icon.png"
+              alt="Iconinteractive"
+              width="200"
+              />
               <i class="line-title white"></i>
               <h2>Learn how to deploy a scalable and effective virtual reality (VR) training solution with a return on investment</h2>
               <p>Presented By ICON Interactive</p>               
               </li>
               <li>
               <img
-              src="https://s3.amazonaws.com/forestry.iconinteractive.com/culture1.jpg"
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/vr-1.png"
               alt="Vr training"
               />
               </li>
             </ul>
           </div>          
         </div>
+
 
         <div class="vr-section-2">
           <div class="container">
@@ -48,6 +54,12 @@
               <p>In the real world, you feel “presence”, which is your subjective experience of reality. Presence allows you to navigate in an environment in which your sensory input responds to your movements and actions. This coordinated system helps your brain determine what is real.</p>
               <p>VR provides a way to deliver enough synchronous sensory input to confuse your brain into thinking that what is happening inside a virtual environment is real. This allows for the benefits of “in-person” interactions to be distributed digitally across your organization.</p>             
               </li>
+              <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/vr-4.png"
+              alt="The Importance of Presence"
+              />
+              </li>
             </ul>            
 
           </div>          
@@ -62,6 +74,12 @@
               <p>VR is highly effective as a training tool due to varying levels of presence. Your ability to retain and recall a subject after a VR experience tends to be higher than after viewing video- or text-based materials.</p>
               <p>Your memory is better when there is increased multi-sensory and emotional input. So when you reflect on a VR experience, it tends to feel more like a memory of something that actually happened.</p>          
               </li>
+              <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/vr-5.png"
+              alt="VR is More Memorable"
+              />
+              </li>
             </ul>              
           </div>          
         </div>
@@ -75,6 +93,12 @@
               <p>In a VR experience, trainees can be repeatedly exposed to a stressful and/or dangerous situation, while under safe conditions. Over time, this can reduce the stress response to that situation. </p>
               <p>Because the experience is virtual, trainees can be subjected to varying levels of intensity allowing them to gradually become accustomed to more intense situations.</p>          
               </li>
+              <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/vr-6.png"
+              alt="VR is Repeatable"
+              />
+              </li>              
             </ul>              
           </div>          
         </div>  
@@ -88,6 +112,12 @@
               <p>VR headsets allow individuals to focus on the task at hand by providing freedom from distractions, while still allowing for virtual collaboration with other trainees and instructors. </p>
               <p>The more engagement and interactivity a trainee experiences, the less likely they are to feel the need to take calls, check messages and scroll through their digital life.</p>          
               </li>
+              <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/vr-7.png"
+              alt="VR Provides Isolation"
+              />
+              </li>                
             </ul>              
           </div>          
         </div>   
@@ -101,6 +131,12 @@
               <p>VR facilitates training in a physical environment without the safety risks to the trainee or expensive equipment. </p>
               <p>If a trainee becomes overwhelmed, the experience can be adjusted to be less overwhelming or they can simply take off the headset.</p> 
               <p>VR allows your trainees to practice before moving on to real-world training situations that have safety risks.</p>         
+              </li>
+              <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/vr-8.png"
+              alt="VR is Safer"
+              />
               </li>
             </ul>              
           </div>          
@@ -129,10 +165,11 @@
         </div>
         <div class="vr-section-10 vr-white">
           <div class="container">
-            <i class="line-title bicolor"></i>
-            <h2>Return on Investment (ROI)</h2>
-            <ul class="grid-two">
+
+            <ul class="grid-two">            
               <li>
+              <i class="line-title bicolor"></i>
+              <h2>Return on Investment (ROI)</h2>                  
                 <p>When calculating the ROI for a VR project, it’s important to consider that VR facilitates <b>learning by doing.</b></p>
                 <p>Edgar Dale’s “cone of learning” contends that after two weeks, the human brain tends to remember only 10% of what it reads and 20% of what it hears, but 75-90% of what it simulates.</p>
                 <h3>Variables for Calculating ROI</h3>
@@ -155,6 +192,10 @@
                 </ul>                
               </li>
               <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/vr-training-9.jpg"
+              alt="VR is Safer"
+              />                
                 <p class="pink">A recent National Training Laboratory study1 stated that retention rates for lecture-style learning were at 5% and reading rates were at 10%, <b>while immersive, hands-on learning had a retention rate of 75%.</b></p>
 
                 <p>Advantage of Simulation in Online Project Management Education, Emergent Research Forum (ERF) April H. Reed, Mark Angolia, East Carolina University. <a href="https://aisel.aisnet.org/cgi/viewcontent.cgi?article=1591&context=amcis2018" target="_blank">Access the full paper from the Association for Information Systems eLibrary here.</a></p>
@@ -171,20 +212,20 @@
         <div class="vr-section-12 vr-white">
           <div class="container">
             <i class="line-title bicolor"></i>
-            <h1>Next Steps</h1>
+            <h2>Next Steps</h2>
             <ul class="grid-three2">
-              <li></li>
+              <li style="background-image:url(https://s3.amazonaws.com/forestry.iconinteractive.com/vr-training-11-a.jpg);"></li>
               <li class="bg_pink"><b>2. Identify Opportunities For Testing</b><br>
                 Once you’ve evaluated your curriculum and understand your goals, begin to identify the gaps in your training and look for areas that could potentially benefit from VR.
               </li>
-              <li></li>
+              <li style="background-image:url(https://s3.amazonaws.com/forestry.iconinteractive.com/vr-training-11-b.jpg);"></li>
             </ul>
             <ul class="grid-three2">
               <li class="bg_pink">
               <b>1. Review Your Curriculum</b><br>
               First up, evaluate your training curriculum. You need to understand where you are and where you’re going BEFORE you decide how you’re going to get there. This first step is critical to your success.
               </li>
-              <li></li>
+              <li style="background-image:url(https://s3.amazonaws.com/forestry.iconinteractive.com/vr-training-11-c.jpg);"></li>
               <li class="bg_pink">
               <b>3. Develop a Pilot Program</b><br>
               Now that you’ve identified areas of opportunity, focus on just one of those for your pilot program. Remember, this is a small-scale, short-term experiment that helps you learn how a large-scale project might work in practice.                
@@ -195,11 +236,11 @@
         <div class="vr-section-13 vr-white">
           <div class="container">
             <i class="line-title bicolor"></i>
-            <h1>Next Steps</h1>
-            <ul class="grid-two2">
+            <h2>Next Steps</h2>
+            <ul class="grid-two">
               <li>
                 <ul class="grid-two2">
-                  <li></li>
+                  <li style="background-image:url(https://s3.amazonaws.com/forestry.iconinteractive.com/vr-training-12-a.jpg);"></li>
                   <li class="bg_pink"><b>5. Scale Fast</b><br>
                     Once you’ve worked out any issues and completed an effective pilot program with measurable results, you’re ready to scale theadvantages of a VR training program across your organization.
                   </li>
@@ -209,10 +250,14 @@
                   <b>4. Measure Results</b><br>
                   Measuring effectiveness is also a critical step. You need to determine which results to measure and how to measure them. This is an important step before scaling, because the last thing you want to do is scale your problems.
                   </li>
-                  <li></li>
+                  <li style="background-image:url(https://s3.amazonaws.com/forestry.iconinteractive.com/vr-training-12-b.jpg);"></li>
                 </ul>                               
               </li>
               <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/vr-12.png"
+              alt="VR is Safer"
+              />                
               </li>
             </ul>
           </div>          
@@ -227,7 +272,12 @@
               <p>VR has the unique advantage over other training methods to generate presence, which leads to more memorable experiences that are repeatable, controllable, isolated from distractions, and safer.</p>
               <p>By working with a partner to identify the appropriate tools and workflows, you can scale up rapidly once the most effective use cases are determined and tested.</p>                
               </li>
-              <li></li>
+              <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/vr-5.png"
+              alt="VR is Safer"
+              />                  
+              </li>
             </ul>
           </div>
         </div>
@@ -240,7 +290,12 @@
               <h3 class="pink">ICON brings together the best of design and technology to create engaging virtual, augmented and mixed reality experiences.</h3>
               <p>With a strong mix of creative, engineering and design talent in Ann Arbor, Boston, and New York, our team is ready to help you <b>plan, design, and develop a virtual reality training experience that delivers better retention rates and increased safety with a measurable return on investment.</b></p>              
               </li>
-              <li></li>
+              <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/vr-training-14.jpg"
+              alt="VR is Safer"
+              />                    
+              </li>
             </ul>
           </div>          
         </div>          
@@ -277,17 +332,23 @@ export default {
     min-height: 85vh;
   }
   .vr-section-2{
-    background:var(--color-highlight);
+
     color: rgb(255, 255, 255);
     padding: 5em 0;
+    background-image:url(https://s3.amazonaws.com/forestry.iconinteractive.com/vr-training-2.jpg);
+    background-size: cover;
   }
   .vr-section-3{
-    background-color:rgba(0,0,0,0.7);
     color: rgb(255, 255, 255);
     padding: 5em 0;    
+    background-image:url(https://s3.amazonaws.com/forestry.iconinteractive.com/vr-training-3.jpg);
+    background-size: cover;    
   }
   .vr-section-11{
     padding: 5em 0; 
+    background-image:url(https://s3.amazonaws.com/forestry.iconinteractive.com/vr-training-10.jpg);
+    background-size: cover;      
+    
   }
   .vr-section-12{
     padding: 5em 0; 
@@ -295,7 +356,8 @@ export default {
   .vr-white{
     background-color:rgba(255,255,255,1);
     color: rgb(0, 0, 0);
-    padding: 5em 0;      
+    padding: 5em 0;
+    border-bottom: 10px solid #ddd;  
   }
   .grid-two {
     display: grid;
@@ -341,6 +403,7 @@ export default {
     background-color:  var(--color-highlight);
     clear: both;
     margin-bottom: 1em;
+    margin-top: 1em;
     display: block;
   }
   .line-title.white{
