7d45ff270bc6b3178ede470d2951d62ae18df1cf
eddy <ovidio43@gmail.com>
2020-03-11 21:28:31 -0400

    edit page

--- src/pages/VR-Training.vue
+++ src/pages/VR-Training.vue
@@ -9,9 +9,16 @@
               <h2>Learn how to deploy a scalable and effective virtual reality (VR) training solution with a return on investment</h2>
               <p>Presented By ICON Interactive</p>               
               </li>
+              <li>
+              <img
+              src="https://s3.amazonaws.com/forestry.iconinteractive.com/culture1.jpg"
+              alt="Vr training"
+              />
+              </li>
             </ul>
           </div>          
         </div>
+
         <div class="vr-section-2">
           <div class="container">
             <i class="line-title white"></i>
@@ -51,7 +58,7 @@
               <li>
              <i class="line-title bicolor"></i>
               <h2>Advantages of VR</h2>
-              <h3>VR is More Memorable</h3>
+              <h3 class="pink">VR is More Memorable</h3>
               <p>VR is highly effective as a training tool due to varying levels of presence. Your ability to retain and recall a subject after a VR experience tends to be higher than after viewing video- or text-based materials.</p>
               <p>Your memory is better when there is increased multi-sensory and emotional input. So when you reflect on a VR experience, it tends to feel more like a memory of something that actually happened.</p>          
               </li>
@@ -64,7 +71,7 @@
               <li>
              <i class="line-title bicolor"></i>
               <h2>Advantages of VR</h2>
-              <h3>VR is Repeatable</h3>
+              <h3 class="pink">VR is Repeatable</h3>
               <p>In a VR experience, trainees can be repeatedly exposed to a stressful and/or dangerous situation, while under safe conditions. Over time, this can reduce the stress response to that situation. </p>
               <p>Because the experience is virtual, trainees can be subjected to varying levels of intensity allowing them to gradually become accustomed to more intense situations.</p>          
               </li>
@@ -77,7 +84,7 @@
               <li>
              <i class="line-title bicolor"></i>
               <h2>Advantages of VR</h2>
-              <h3>VR Provides Isolation From Distractions</h3>
+              <h3 class="pink">VR Provides Isolation From Distractions</h3>
               <p>VR headsets allow individuals to focus on the task at hand by providing freedom from distractions, while still allowing for virtual collaboration with other trainees and instructors. </p>
               <p>The more engagement and interactivity a trainee experiences, the less likely they are to feel the need to take calls, check messages and scroll through their digital life.</p>          
               </li>
@@ -90,7 +97,7 @@
               <li>
              <i class="line-title bicolor"></i>
               <h2>Advantages of VR</h2>
-              <h3>VR is Safer</h3>
+              <h3 class="pink">VR is Safer</h3>
               <p>VR facilitates training in a physical environment without the safety risks to the trainee or expensive equipment. </p>
               <p>If a trainee becomes overwhelmed, the experience can be adjusted to be less overwhelming or they can simply take off the headset.</p> 
               <p>VR allows your trainees to practice before moving on to real-world training situations that have safety risks.</p>         
@@ -104,46 +111,142 @@
             <h2>Skills Suited For VR Training</h2>
             <ul class="grid-three">
               <li>
-                <h3>Cognitive Skills</h3>
-<p>Cognitive skills are the core skills your brain uses to think, read, learn, remember, reason, and pay attention.</p>
-<p>Working together, they take incoming information and move it into the bank of knowledge you use every day.</p>
+                <h3 class="pink">Cognitive Skills</h3>
+                <p>Cognitive skills are the core skills your brain uses to think, read, learn, remember, reason, and pay attention.</p>
+                <p>Working together, they take incoming information and move it into the bank of knowledge you use every day.</p></li>
+              <li>
+                <h3 class="pink">Psychomotor Skills</h3>
+                <p>Psychomotor skills represent the relationship between cognitive functions and physical movement.</p>
+                <p>Psychomotor learning is demonstrated by physical skills such as movement, coordination, manipulation, dexterity, grace, strength and speed.</p>
               </li>
               <li>
-<h3>Psychomotor Skills</h3>
-<p>Psychomotor skills represent the relationship between cognitive functions and physical movement.</p>
-<p>Psychomotor learning is demonstrated by physical skills such as movement, coordination, manipulation, dexterity, grace, strength and speed.</p>
-                
+                <h3 class="pink">Soft Skills</h3>
+                <p>Soft skills are the interpersonal and social skills needed to control feelings and emotions.</p>
+                <p>Practicing real-world situations in the safety of a virtual world allows trainees to learn through experience. </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="vr-section-10 vr-white">
+          <div class="container">
+            <i class="line-title bicolor"></i>
+            <h2>Return on Investment (ROI)</h2>
+            <ul class="grid-two">
+              <li>
+                <p>When calculating the ROI for a VR project, it’s important to consider that VR facilitates <b>learning by doing.</b></p>
+                <p>Edgar Dale’s “cone of learning” contends that after two weeks, the human brain tends to remember only 10% of what it reads and 20% of what it hears, but 75-90% of what it simulates.</p>
+                <h3>Variables for Calculating ROI</h3>
+                <ul class="grid-two">
+                  <li>
+                  <b>Cost Variables</b><br>
+                  Development<br>
+                  Installation<br>
+                  Licensing Per Site<br>
+                  Sustainability<br>
+                  Training Room<br>
+                  Training Time                    
+                  </li>
+                  <li>
+                  <b>Benefit Variables</b><br>
+                  Incidents Deduced Per Course<br>
+                  Cost Incurred Per Incident<br>
+                  Incident Savings Per Course                    
+                  </li>
+                </ul>                
               </li>
               <li>
-<h3>Soft Skills</h3>
-<p>Soft skills are the interpersonal and social skills needed to control feelings and emotions.</p>
-<p>Practicing real-world situations in the safety of a virtual world allows trainees to learn through experience. </p>               
+                <p class="pink">A recent National Training Laboratory study1 stated that retention rates for lecture-style learning were at 5% and reading rates were at 10%, <b>while immersive, hands-on learning had a retention rate of 75%.</b></p>
+
+                <p>Advantage of Simulation in Online Project Management Education, Emergent Research Forum (ERF) April H. Reed, Mark Angolia, East Carolina University. <a href="https://aisel.aisnet.org/cgi/viewcontent.cgi?article=1591&context=amcis2018" target="_blank">Access the full paper from the Association for Information Systems eLibrary here.</a></p>
               </li>
             </ul>
           </div>          
-        </div>                                                           
+        </div>
+        <div class="vr-section-11">
+          <div class="container">
+            <i class="line-title bicolor"></i>
+            <h2>Now that we’ve covered some of the fundamental ways VR is effective for training, we’ll outline the actionable steps you can take to leverage this opportunity.</h2>
+          </div>          
+        </div>
+        <div class="vr-section-12 vr-white">
+          <div class="container">
+            <i class="line-title bicolor"></i>
+            <h1>Next Steps</h1>
+            <ul class="grid-three2">
+              <li></li>
+              <li class="bg_pink"><b>2. Identify Opportunities For Testing</b><br>
+                Once you’ve evaluated your curriculum and understand your goals, begin to identify the gaps in your training and look for areas that could potentially benefit from VR.
+              </li>
+              <li></li>
+            </ul>
+            <ul class="grid-three2">
+              <li class="bg_pink">
+              <b>1. Review Your Curriculum</b><br>
+              First up, evaluate your training curriculum. You need to understand where you are and where you’re going BEFORE you decide how you’re going to get there. This first step is critical to your success.
+              </li>
+              <li></li>
+              <li class="bg_pink">
+              <b>3. Develop a Pilot Program</b><br>
+              Now that you’ve identified areas of opportunity, focus on just one of those for your pilot program. Remember, this is a small-scale, short-term experiment that helps you learn how a large-scale project might work in practice.                
+              </li>
+            </ul>            
+          </div>          
+        </div>
+        <div class="vr-section-13 vr-white">
+          <div class="container">
+            <i class="line-title bicolor"></i>
+            <h1>Next Steps</h1>
+            <ul class="grid-two2">
+              <li>
+                <ul class="grid-two2">
+                  <li></li>
+                  <li class="bg_pink"><b>5. Scale Fast</b><br>
+                    Once you’ve worked out any issues and completed an effective pilot program with measurable results, you’re ready to scale theadvantages of a VR training program across your organization.
+                  </li>
+                </ul> 
+                <ul class="grid-two2">
+                  <li class="bg_pink">
+                  <b>4. Measure Results</b><br>
+                  Measuring effectiveness is also a critical step. You need to determine which results to measure and how to measure them. This is an important step before scaling, because the last thing you want to do is scale your problems.
+                  </li>
+                  <li></li>
+                </ul>                               
+              </li>
+              <li>
+              </li>
+            </ul>
+          </div>          
+        </div> 
+        <div class="vr-section-14 vr-white">
+          <div class="container">
+            <ul class="grid-two">
+              <li>
+              <i class="line-title bicolor"></i>
+              <h2>Conclusion</h2>
+              <h3>Memorable Matters</h3>
+              <p>VR has the unique advantage over other training methods to generate presence, which leads to more memorable experiences that are repeatable, controllable, isolated from distractions, and safer.</p>
+              <p>By working with a partner to identify the appropriate tools and workflows, you can scale up rapidly once the most effective use cases are determined and tested.</p>                
+              </li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+        <div class="vr-section-15 vr-white">
+          <div class="container">
+            <ul class="grid-two">
+              <li>
+              <i class="line-title bicolor"></i>
+              <h2>How We Can Help</h2>
+              <h3 class="pink">ICON brings together the best of design and technology to create engaging virtual, augmented and mixed reality experiences.</h3>
+              <p>With a strong mix of creative, engineering and design talent in Ann Arbor, Boston, and New York, our team is ready to help you <b>plan, design, and develop a virtual reality training experience that delivers better retention rates and increased safety with a measurable return on investment.</b></p>              
+              </li>
+              <li></li>
+            </ul>
+          </div>          
+        </div>          
       </section>
 
 
-
-                       
-
-      <section class="vr-section-7">
-        <div class="container">
-        </div>          
-      </section>
-      <section class="vr-section-8">
-        <div class="container">
-        </div>          
-      </section>
-      <section class="vr-section-9">
-        <div class="container">
-        </div>          
-      </section>
-      <section class="vr-section-10">
-        <div class="container">
-        </div>          
-      </section>
       <section class="vr-section-11">
         <div class="container">
         </div>          
@@ -154,10 +257,18 @@
 </template>
 
 <script>
-export default {};
+export default {
+
+};
 </script>
 
-<style>
+<style scoped>
+  .pink{
+    color: var(--color-highlight);
+  }
+  .bg_pink{
+    background-color: var(--color-highlight);
+  }
   .vr-section-1{
     color: rgb(255, 255, 255);
     background:var(--color-highlight);
@@ -174,6 +285,12 @@ export default {};
     background-color:rgba(0,0,0,0.7);
     color: rgb(255, 255, 255);
     padding: 5em 0;    
+  }
+  .vr-section-11{
+    padding: 5em 0; 
+  }
+  .vr-section-12{
+    padding: 5em 0; 
   }
   .vr-white{
     background-color:rgba(255,255,255,1);
@@ -194,6 +311,30 @@ export default {};
     list-style: none;
     padding: 0;
   }  
+  .grid-three2 {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 0.1rem;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  .grid-two2 {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 0.1rem;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }    
+  .grid-three2 li{
+    padding: 2em;
+    color: white;
+  }
+  .grid-two2 li{
+    padding: 1em;
+    color: white;    
+  }
   .line-title{
     height: 8px;
     width: 100px;
