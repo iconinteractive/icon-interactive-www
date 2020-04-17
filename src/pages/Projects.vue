e92a943be37155a014221222fa3edae889d48a9e
daltn <daltonsaffe@gmail.com>
2020-04-17 12:38:12 -0400

    mod link

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -9,9 +9,9 @@
       <ProjectsGrid :projects="$page.projects.edges" />
       <LogoGridProjects />
       <div class="see-more-container">
-        <g-link class="see-more" to="/clients">
+        <a class="see-more" href="/clients/">
           <h3>See More</h3>
-        </g-link>
+        </a>
       </div>
     </div>
   </Layout>
