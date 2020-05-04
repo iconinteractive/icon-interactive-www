d11eed345ae1cf6d1057b1b1c0cf3b4443680105
daltn <daltonsaffe@gmail.com>
2020-05-04 17:15:24 -0400

    mod blocks update wip

--- src/templates/BlocksPage.vue
+++ src/templates/BlocksPage.vue
@@ -4,6 +4,12 @@
       <h1>Blocks Page</h1>
       <div v-html="$page.post.content" />
       <h1>{{ $page.post.project_blocks}}</h1>
+      <p>{{ $page.post}}</p>
+      <component
+        v-for="(item, idx) in $page.post.project_blocks"
+        v-bind:is="item.template"
+        :key="idx"
+      ></component>
     </div>
   </Layout>
 </template>
@@ -14,22 +20,27 @@ query BlocksPage ($path: String!) {
   post: blocksPage (path: $path) {
    project_blocks {
     template
-    full_width_image
     quote
-  } 
+    } 
   }
 }
 </page-query>
 
 <script>
+import full_width_image from "../components/FullWidthImage";
+import quote from "../components/Quote";
+
 export default {
-  components: {}
+  components: {
+    full_width_image,
+    quote
+  }
   // metaInfo() {
   //   return {
   //     title: this.$page.post.title
   //   };
   // }
-}
+};
 </script>
 
 <style scoped>
--- work/testingworkproject.md
+++ work/testingworkproject.md
@@ -1,10 +1,9 @@
 ---
 project_blocks:
-- template: image
-  full_width_image: https://s3.amazonaws.com/forestry.iconinteractive.com/Panasonic.008.jpeg
-- template: quote
-  quote: 'test quote one two. hey '
-- template: quote
-  quote: ''
-
+  - template: full_width_image
+    full_width_image: https://s3.amazonaws.com/forestry.iconinteractive.com/Panasonic.008.jpeg
+  - template: quote
+    quote: "test quote one two. hey "
+  - template: quote
+    quote: ""
 ---
