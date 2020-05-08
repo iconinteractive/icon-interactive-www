284f0a51780c8412a7fbd8d181d2176d377170d5
Adam Riggs <adam@Admins-MacBook-Pro-4.local>
2020-05-08 14:48:42 -0400

    results beginning

---
+++ src/templates/Results.vue
@@ -0,0 +1,16 @@
+<template>
+<section>
+<h2>Results</h2>
+  <!-- <blockquote v-if="$page.post.pull_quote" v-html="$page.post.pull_quote" /> -->
+</section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
