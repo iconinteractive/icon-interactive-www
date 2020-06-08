555da890c6d9a91f2b306085e429941216328b49
daltn <daltonsaffe@gmail.com>
2020-06-08 16:37:12 -0400

    mod fbook360

--- src/components/Facebook360.vue
+++ src/components/Facebook360.vue
@@ -1,6 +1,6 @@
 <template>
-  <section>
-    <div v-if="isMobile" class="embed margin">
+  <section v-if="isMobile" class="facebook360">
+    <div class="embed margin">
       <iframe
         :src="facebook360block.facebook_360_src_url"
         class="full-width"
@@ -12,7 +12,6 @@
         title="facebook 360 video"
       ></iframe>
     </div>
-    <div v-else class="hide" />
   </section>
 </template>
 
@@ -27,17 +26,26 @@ export default {
   methods: {
     isMobile() {
       if (process.isClient) {
-        if (navigator.userAgent.match(/Android/i) ||
-        navigator.userAgent.match(/webOS/i)||
-        navigator.userAgent.match(/iPhone/i)||
-        navigator.userAgent.match(/iPad/i)||
-        navigator.userAgent.match(/iPod/i)||
-        navigator.userAgent.match(/BlackBerry/i)||
-        navigator.userAgent.match(/Windows Phone/i)) {
-        this.mobileCheck = true
-      } else {
-        this.mobileCheck = false
-      }
+        let hasTouchScreen = false;
+        if ("maxTouchPoints" in navigator) {
+            hasTouchScreen = navigator.maxTouchPoints > 0;
+        } else if ("msMaxTouchPoints" in navigator) {
+            hasTouchScreen = navigator.msMaxTouchPoints > 0;
+        } else {
+            var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+            if (mQ && mQ.media === "(pointer:coarse)") {
+                hasTouchScreen = !!mQ.matches;
+            } else if ('orientation' in window) {
+                hasTouchScreen = true;
+            } else {
+                let UA = navigator.userAgent;
+                hasTouchScreen = (
+                    /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+                    /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+                );
+            }
+            this.mobileCheck = hasTouchScreen
+        }
       }
     },
     created() {
@@ -63,4 +71,9 @@ export default {
 .hide {
   display: none;
 }
+/* @media (max-width: 800px) {
+  .facebook360 {
+    display: none;
+  }
+} */
 </style>
