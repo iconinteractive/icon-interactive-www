530a7f73773129572a7203730ded7832de0d4504
dalton.s <dalton.s@iconinteractive.com>
2020-06-16 18:15:11 -0400

    mod news posts

--- src/components/EventContent.vue
+++ src/components/EventContent.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="journal-content" v-html="content" />
+  <section class="text-wrap margin">
+    <div class="text-block-news" v-html="content" />
+  </section>
 </template>
 
 <script>
 export default {
   props: {
-    content: String
-  }
-};
+    content: String,
+  },
+}
 </script>
 
 <style>
@@ -21,5 +23,54 @@ h4,
 h5,
 h6 {
   font-size: 1.15rem;
+}
+.text-wrap {
+  display: flex;
+  justify-content: center;
+  margin: 5rem 0 0;
+  color: var(--color-contrast);
+}
+.text-block-news {
+  width: 70%;
+  font-size: 1.1rem;
+  line-height: 1.6rem;
+  max-width: 700px;
+  display: flex;
+  flex-direction: column;
+}
+.text-block-news img {
+  display: block;
+  margin: 0 auto;
+}
+.text-block-news h1,
+.text-block-news h2,
+.text-block-news h3,
+.text-block-news h4,
+.text-block-news h5,
+.text-block-news h6 {
+  font-family: "DM Sans", Arial, Helvetica, sans-serif;
+  font-weight: normal;
+  color: var(--color-highlight);
+  margin: 0;
+}
+.text-block-news p {
+  margin: 0.5rem 0 2rem;
+}
+ul {
+  padding-left: 20px;
+  margin: 0;
+}
+.margin {
+  margin-left: 6rem;
+  margin-right: 6rem;
+}
+@media (max-width: 800px) {
+  .text-block-news {
+    width: 100%;
+  }
+  .margin {
+    margin-left: 2rem;
+    margin-right: 2rem;
+  }
 }
 </style>
--- src/templates/NewsPost.vue
+++ src/templates/NewsPost.vue
@@ -1,8 +1,12 @@
 <template>
   <Layout>
-    <div class="container">
-      <h1 v-html="$page.post.title" class="title" />
-      <EventContent class="event-content" :content="$page.post.content" />
+    <div :style="style">
+      <h1 v-html="$page.post.title" class="news-title" />
+      <EventContent
+        :style="style"
+        class="event-content"
+        :content="$page.post.content"
+      />
     </div>
   </Layout>
 </template>
@@ -18,18 +22,26 @@ query NewsPost ($path: String!) {
 </page-query>
 
 <script>
-import EventContent from "@/components/EventContent";
+import EventContent from "@/components/EventContent"
 
 export default {
   components: {
-    EventContent
+    EventContent,
+  },
+  computed: {
+    style() {
+      return {
+        backgroundColor: `#f7f7f7`,
+        color: "#000",
+      }
+    },
   },
   metaInfo() {
     return {
-      title: this.$page.post.title
-    };
-  }
-};
+      title: this.$page.post.title,
+    }
+  },
+}
 </script>
 
 <style scoped>
@@ -37,35 +49,13 @@ a {
   text-decoration: underline;
   color: var(--color-highlight);
 }
-.event-content {
-  width: 75%;
-}
-.event-header {
-  padding: 2rem 0 4rem 0;
-}
-.title {
-  margin: 0 0 4rem 0;
-  padding: 0;
-  line-height: 5.25rem;
-}
-.event-meta {
-  display: flex;
-  flex-wrap: wrap;
-  font-size: 1rem;
-}
-.event-meta > div {
-  margin-right: 4rem;
-}
-.event-meta > div:last-of-type {
-  margin: 0;
-}
-.label {
-  color: var(--color-highlight);
+.news-title {
+  text-align: center;
+  font-size: 4rem;
+  width: 80%;
+  margin: 0 auto;
 }
 @media (max-width: 800px) {
-  .event-content {
-    width: 95%;
-  }
   .title {
     line-height: 3rem;
   }
